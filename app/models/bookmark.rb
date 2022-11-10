5ac47d9392790d9060b581b8a06620989471a560
Elyse Turpin <turpin.elyse@gmail.com>
2022-11-10 11:54:43 -0500

    associations and validations

---
+++ app/models/bookmark.rb
@@ -0,0 +1,12 @@
+class Bookmark < ApplicationRecord
+  belongs_to :movie
+  belongs_to :list
+
+  validates :movie_id, uniqueness: { scope: :list_id,
+  message: "that movie is already in that list" }
+
+  validates :list_id, uniqueness: { scope: :movie_id,
+  message: "that list already contains that movie" }
+
+  validates :comment, length: { minimum: 6 }
+end
