2f8211d9e9b9d9a6f67adf9764dc6445d5b1e467
Elyse Turpin <turpin.elyse@gmail.com>
2022-11-10 13:22:16 -0500

    updated seedfile so image links work

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-
-
 require 'open-uri'
 require 'json'
+puts "cleaning the DB..."
+Movie.destroy_all
+puts "DB is clean."
 
 puts "seeding..."
 
@@ -18,7 +12,12 @@ movies_array = JSON.parse(movies_serialized)
 movies = movies_array['results']
 
 movies.sample(10).each do |movie|
-  Movie.create(title: movie['title'], overview: movie['overview'], poster_url: movie['poster_path'], rating: movie['vote_average'])
+  Movie.create(
+    title: movie['title'],
+    overview: movie['overview'],
+    poster_url: "https://image.tmdb.org/t/p/w500#{movie['poster_path']}",
+    rating: movie['vote_average']
+  )
 end
 
-puts "seeded."
+puts "#{Movie.count} movie(s) seeded."
