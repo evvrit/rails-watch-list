b1be0969afa177f965490319d694378dc9ef9f61
Elyse Turpin <turpin.elyse@gmail.com>
2022-11-10 16:50:37 -0500

    made a navbar and started making a bookmark card

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,7 +15,7 @@ movies.sample(10).each do |movie|
   Movie.create(
     title: movie['title'],
     overview: movie['overview'],
-    poster_url: "https://image.tmdb.org/t/p/w500#{movie['poster_path']}",
+    poster_url: "https://image.tmdb.org/t/p/#{movie['poster_path']}",
     rating: movie['vote_average']
   )
 end
