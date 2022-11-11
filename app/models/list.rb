4a151774687b93890f5dcfa9dd07e7e255a8444d
Elyse Turpin <turpin.elyse@gmail.com>
2022-11-11 15:16:17 -0500

    added image functionality for lists

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,5 +2,7 @@ class List < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
   has_many :movies, through: :bookmarks
 
+  has_one_attached :photo
+
   validates :name, presence: true, uniqueness: true
 end
