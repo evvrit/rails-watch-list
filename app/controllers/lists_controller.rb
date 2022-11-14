de969b4a5a5ea72ff07b4328876af5ec59936935
Elyse Turpin <turpin.elyse@gmail.com>
2022-11-13 20:26:32 -0500

    image tag not working

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -17,7 +17,7 @@ class ListsController < ApplicationController
     if @list.save
       redirect_to list_path(@list)
     else
-      render :new, status: unprocessable_entity
+      render :new, status: :unprocessable_entity
     end
   end
 
