5e5b7d115e481146e3aa252ad78c96af2c20f7ef
Elyse Turpin <turpin.elyse@gmail.com>
2022-11-14 09:35:49 -0500

    made some styling changes

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -12,7 +12,7 @@ class BookmarksController < ApplicationController
     if @bookmark.save
       redirect_to list_path(@list)
     else
-      render :new, status: unprocessable_entity
+      render :new, status: :unprocessable_entity
     end
   end
 
--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -3,10 +3,10 @@ class Bookmark < ApplicationRecord
   belongs_to :list
 
   validates :movie_id, uniqueness: { scope: :list_id,
-  message: "that movie is already in that list" }
+  message: "--that movie is already in that list" }
 
   validates :list_id, uniqueness: { scope: :movie_id,
-  message: "that list already contains that movie" }
+  message: " --that list already contains that movie" }
 
   validates :comment, length: { minimum: 6 }
 end
